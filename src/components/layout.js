e38bf2a81b70508c91477238b9896fca48f5e238
Mario Garcia <msgimanila@yahoo.com>
2023-10-07 20:52:07 +0800

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import Chatbox from "chatbox"
+import Chatbox from "../components/chatbox"
+
 const Layout = ({ location, title, children }) => {
   
   const rootPath = `${__PATH_PREFIX__}/`
