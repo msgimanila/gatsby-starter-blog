fe9120ce638b98d6cb89e7f631eb046e53f732d6
Mario Garcia <msgimanila@yahoo.com>
2023-10-07 21:00:39 +0800

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import Chatbox from "ChatBox"
+import Chatbox from "chatbox"
 
 const Layout = ({ location, title, children }) => {
   
